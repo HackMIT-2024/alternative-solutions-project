dcdf21d212f31abe6cb2d0de6b2aefc1b0684ab1
Elaine Wang <elaine.liuwang@gmail.com>
2024-09-14 17:14:24 -0400

    search bar basic with submit, need to handle submit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,18 @@
+'use client';
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import SearchBar from "./components/SearchBar"
 
 export default function Home() {
+  const handleSearch = (query: unknown) => {
+    console.log('Search:', query);
+    // Implement search logic here
+  };
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+      <SearchBar onSearch={ handleSearch } ></SearchBar>
       </main>
       <footer className={styles.footer}>
         <a
