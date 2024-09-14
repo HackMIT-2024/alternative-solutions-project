447b764e64f637b715cd483dd6a2cad0791295ac
hcshires <apradhan7678@gmail.com>
2024-09-14 19:25:46 -0400

    add schemas to the convex tables

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,6 +1,18 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
+const businesses = defineTable({
+  url: v.string(),
+  name: v.string(),
+});
+
+const products = defineTable({
+  description: v.string(),
+  name: v.string(),
+  businessId: v.id("businesses"),
+  url: v.string(),
+});
+
 const users = defineTable({
   name: v.string(),
   // this the Clerk ID, stored in the subject JWT field
@@ -8,5 +20,7 @@ const users = defineTable({
 }).index("byExternalId", ["externalId"]);
 
 export default defineSchema({
+  businesses,
+  products,
   users,
 });
